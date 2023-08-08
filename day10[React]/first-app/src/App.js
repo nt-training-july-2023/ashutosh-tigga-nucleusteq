58cfbcd9ee306491e94edab655ffbfee783b9cf8
Ashu-Nucleusteq <ashutosh.tigga@nucleusteq.com>
2023-08-08 11:25:19 +0530

    updated data are dynamic showing in page in register

--- day10[React]/first-app/src/App.js
+++ day10[React]/first-app/src/App.js
@@ -5,8 +5,8 @@ import Register from './component/Register';
 function App() {
   return (
     <div className="App">
-      {/* <Body/> */}
-      <Register/>
+      <Body/> 
+      {/* <Register/> */}
     </div>
   );
 }
--- day10[React]/first-app/src/component/Body.jsx
+++ day10[React]/first-app/src/component/Body.jsx
@@ -9,17 +9,32 @@ const Body = () => {
     const [gender, setGender] = useState("male");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [allEntry, setAllEntry] = useState([]);
 
     const ValidateName = () => {
         console.log(`full name : ${fullName}`);
-        const name=fullName.trim().toLowerCase();
+        const name = fullName.trim().toLowerCase();
         if (name.length <= 5) {
             window.alert(`Your name length must be greater than 5 !! \n Please enter correctly`)
         }
         for (let i = 0; i < name.length; i++) {
             let ch = name.charAt(i);
-            if ((ch >= 0 && ch <= 9) || (ch == "!") || (ch == "@") || (ch == "#") || (ch == "$") || (ch == "%") ||
-                (ch == "^") || (ch == "&") || (ch == "*") || (ch == "(") || (ch == ")") || (ch == "_") || (ch == "-") || (ch == "+") || (ch == "=")) {
+            if ((ch >= 0 && ch <= 9) ||
+                (ch == "!") ||
+                (ch == "@") ||
+                (ch == "#") ||
+                (ch == "$") ||
+                (ch == "%") ||
+                (ch == "^") ||
+                (ch == "&") ||
+                (ch == "*") ||
+                (ch == "(") ||
+                (ch == ")") ||
+                (ch == "_") ||
+                (ch == "-") ||
+                (ch == "+") ||
+                (ch == "=") ||
+                (ch == " ")) {
                 window.alert(`It must not contains this ${ch} !! \nIt must contains alphabets `);
             }//end if
         }//end for
@@ -32,12 +47,17 @@ const Body = () => {
         } else if (userName.length >= 8) {
             for (let i = 0; i < userName.length; i++) {
                 let ch = userName.charAt(i);
-                let valid = (ch >= 0) && (ch <= 9) || (ch >= 'a') && (ch <= 'z') || (ch >= 'A') && (ch <= 'Z')
-                    (ch == '@') || (ch == '$') || (ch == '_') || (ch == '#');
+                let valid = (ch >= 0) && (ch <= 9) ||
+                    (ch >= 'a') && (ch <= 'z') ||
+                    (ch >= 'A') && (ch <= 'Z') ||
+                    (ch == '@') ||
+                    (ch == '$') ||
+                    (ch == '_') ||
+                    (ch == '#');
                 if (valid) {
 
                 } else {
-                    console.log("user name length should be of min 8 length \n it must contains combination and numeric and alhabets \n you can also use speacial keyword like @ , # , _ ");
+                    alert("user name length should be of min 8 length \n it must contains combination and numeric and alhabets \n you can also use speacial keyword like @ , # , _ ");
                 }//end if else inner
             }//end for
         }//end if else outer
@@ -52,12 +72,16 @@ const Body = () => {
         } else if (user.length > 5) {
             for (let i = 0; i < user.length; i++) {
                 let ch = user.charAt(i);
-                let valid = (ch >= 0) && (ch <= 9) || (ch >= 'a') && (ch <= 'z') || (ch >= 'A') && (ch <= 'Z')
-                    (ch == '@') || (ch == '$') || (ch == '_') || (ch == '#');
+                let valid = (ch >= 0) && (ch <= 9) ||
+                    (ch >= 'a') && (ch <= 'z') ||
+                    (ch >= 'A') && (ch <= 'Z') ||
+                    (ch == '@') || (ch == '$') ||
+                    (ch == '_') || (ch == '#') ||
+                    (ch == '.');
                 if (valid) {
                     console.log("valid")
                 } else {
-                    console.log("email user name is not valid it must contains and alpphabets , numberic and special character @ , $ , _");
+                    alert("email user name is not valid it must contains and alpphabets , numberic and special character @ , $ , _");
                 }//end if else
             }
             if (emailLowerCase.includes("@gmail.com") || emailLowerCase.includes("@hotmail.com") || emailLowerCase.includes("@nucleusteq.com") || emailLowerCase.includes("@outlook.com")) {
@@ -92,92 +116,114 @@ const Body = () => {
         console.log(`confirm password : ${confirmPassword}`);
     }
 
-    const submitData = () => {
+    const save = (e) => {
+        e.preventDefault();
+        const newEntry = { fullName: fullName, userName: userName, email: email, number: number, gender: gender, password: password };
+        setAllEntry([...allEntry, newEntry]);
+        console.log(`all entry :${allEntry}`)
+    }
+
+    const submitData = (e) => {
         ValidateName();
         ValidateUserName();
         ValidateEmail();
         ValidateNumber();
         ValidatePassword();
         VaidateConfirmPassword();
-
+        save(e);
     }
     return (
-        <div id="body">
-            <div id="main_div">
+        <>
+            <div id="body">
+                <div id="main_div">
 
-                {/* <!--start of header div--> */}
-                <div id="header_div">
-                    <h2>Registration</h2>
-                    <hr id="horizontal_line" />
-                </div>
-                {/* <!--end of header div--> */}
-
-                {/* <!--start of body div--> */}
-                <div class="body_div">
-                    <div id="input_div1">
-                        <div class="input_div">
-                            <label for="Full Name">Full Name</label><br />
-                            <input name="fullName" value={fullName} class="input" onChange={(e) => { setFullName(e.target.value) }} type="text" placeholder="Enter your name" required />
-                        </div>
-                        <div class="input_div">
-                            <label for="Username">Username</label><br />
-                            <input name="userName" value={userName} class="input" onChange={(e) => { setUserName(e.target.value) }} type="text" placeholder="Enter your username" required />
-                        </div>
+                    {/* <!--start of header div--> */}
+                    <div id="header_div">
+                        <h2>Registration</h2>
+                        <hr id="horizontal_line" />
                     </div>
-                    {/* <!--end of 1st input block--> */}
+                    {/* <!--end of header div--> */}
 
-                    <div id="input_div2">
-                        <div class="input_div">
-                            <label for="Email">Email</label><br />
-                            <input name="email" value={email} class="input" onChange={(e) => { setEmail(e.target.value) }} type="text" placeholder="Enter your email" required />
-                        </div>
-                        <div class="input_div">
-                            <label for="Number">Phone Number</label><br />
-                            <input name="number" value={number} class="input" onChange={(e) => { setNumber(e.target.value) }} type="text" placeholder="Enter your number" required />
-                        </div>
-                    </div>
-                    {/* <!--end of 2nd input block--> */}
-
-                    <div id="input_div3">
-                        <div class="input_div">
-                            <label for="Password">Password</label><br />
-                            <input name="password" value={password} class="input" onChange={(e) => { setPassword(e.target.value) }} type="text" placeholder="Enter your password" required />
-                        </div>
-                        <div class="input_div">
-                            <label for="Confirmpassword">Confirm Password</label><br />
-                            <input name="confirmPassword" value={confirmPassword} class="input" onChange={(e) => { setConfirmPassword(e.target.value) }} type="text" placeholder="Confirm your password" required />
-                        </div>
-                    </div>
-                    {/* <!--end of 3rd input block--> */}
-
-                    <div id="input_div4">
-                        <label>Gender</label><br />
-                        <div id="input_sub_div4">
+                    {/* <!--start of body div--> */}
+                    <div class="body_div">
+                        <div id="input_div1">
                             <div class="input_div">
-                                <input type="radio" value={gender} onChange={(e) => { setGender(e.target.value) }} name="gender" class="radio_input" required />
-                                <label for="Male">Male</label>
+                                <label for="Full Name">Full Name</label><br />
+                                <input name="fullName" value={fullName} class="input" onChange={(e) => { setFullName(e.target.value) }} type="text" placeholder="Enter your name" required />
                             </div>
                             <div class="input_div">
-                                <input type="radio" value={gender} onChange={(e) => { setGender(e.target.value) }} name="gender" class="radio_input" required />
-                                <label for="Female">Female</label>
-                            </div>
-                            <div class="input_div">
-                                <input type="radio" value={gender} onChange={(e) => { setGender(e.target.value) }} name="gender" class="radio_input" required />
-                                <label for="no">Prefer not to say</label>
+                                <label for="Username">Username</label><br />
+                                <input name="userName" value={userName} class="input" onChange={(e) => { setUserName(e.target.value) }} type="text" placeholder="Enter your username" required />
                             </div>
                         </div>
-                    </div>
-                    {/* <!--end of 4th input block--> */}
-                </div>
-                {/* <!--end of body_div block--> */}
+                        {/* <!--end of 1st input block--> */}
 
-                {/* <!-- start of footer div--> */}
-                <div id="footer_div">
-                    <input type="button" id="input_button" value="Register" onClick={submitData} />
+                        <div id="input_div2">
+                            <div class="input_div">
+                                <label for="Email">Email</label><br />
+                                <input name="email" value={email} class="input" onChange={(e) => { setEmail(e.target.value) }} type="text" placeholder="Enter your email" required />
+                            </div>
+                            <div class="input_div">
+                                <label for="Number">Phone Number</label><br />
+                                <input name="number" value={number} class="input" onChange={(e) => { setNumber(e.target.value) }} type="text" placeholder="Enter your number" required />
+                            </div>
+                        </div>
+                        {/* <!--end of 2nd input block--> */}
+
+                        <div id="input_div3">
+                            <div class="input_div">
+                                <label for="Password">Password</label><br />
+                                <input name="password" value={password} class="input" onChange={(e) => { setPassword(e.target.value) }} type="text" placeholder="Enter your password" required />
+                            </div>
+                            <div class="input_div">
+                                <label for="Confirmpassword">Confirm Password</label><br />
+                                <input name="confirmPassword" value={confirmPassword} class="input" onChange={(e) => { setConfirmPassword(e.target.value) }} type="text" placeholder="Confirm your password" required />
+                            </div>
+                        </div>
+                        {/* <!--end of 3rd input block--> */}
+
+                        <div id="input_div4">
+                            <label>Gender</label><br />
+                            <div id="input_sub_div4">
+                                <div class="input_div">
+                                    <input type="radio" value={gender} onChange={(e) => { setGender(e.target.value) }} name="gender" class="radio_input" required />
+                                    <label for="Male">Male</label>
+                                </div>
+                                <div class="input_div">
+                                    <input type="radio" value={gender} onChange={(e) => { setGender(e.target.value) }} name="gender" class="radio_input" required />
+                                    <label for="Female">Female</label>
+                                </div>
+                                <div class="input_div">
+                                    <input type="radio" value={gender} onChange={(e) => { setGender(e.target.value) }} name="gender" class="radio_input" required />
+                                    <label for="no">Prefer not to say</label>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!--end of 4th input block--> */}
+                    </div>
+                    {/* <!--end of body_div block--> */}
+
+                    {/* <!-- start of footer div--> */}
+                    <div id="footer_div">
+                        <input type="button" id="input_button" value="Register" onClick={(e) => { submitData(e) }} />
+                    </div>
+                    {/* <!--End of footer div--> */}
                 </div>
-                {/* <!--End of footer div--> */}
             </div>
-        </div>
+            <div >
+                {
+                    allEntry.map((currElement) => {
+                        return (
+                            <div className="showData">
+                                <p>   Full Name : <span>{currElement.fullName} </span></p>
+                                <p>   Email : <span>{currElement.email}</span> </p>
+                                <p>   Username : <span>{currElement.userName}</span> </p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     )//return end
 };
 
